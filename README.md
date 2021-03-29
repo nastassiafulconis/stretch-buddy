96760fde4456010f20c8ab7d9a16f08665d39c6b
Nastassia Fulconis <nastassia.fulconis@gmail.com>
2021-03-29 17:40:21 -0400

    initial environment setup complete

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# stretch-buddy
-A stretching tool build on React Hooks, Express, and Node.js using the Youtube API 
+# Stretch Buddy
+A stretching tool build on React Hooks, Express, and Node.js using the Youtube API
---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
