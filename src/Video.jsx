fef28a476fb07cdfa691c5049831cc3c365c66b3
Nastassia Fulconis <nastassia.fulconis@gmail.com>
2021-03-31 09:14:24 -0400

    styled modal window

--- src/Video.jsx
+++ src/Video.jsx
@@ -10,6 +10,10 @@ const customStyles = {
     bottom: 'auto',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
+    width: '50%',
+    height: '50%',
+    borderRadius: '20px',
+    boxShadow: '0.5px 0.5px 5px gray'
   }
 };
 
@@ -38,10 +42,12 @@ const Video = forwardRef(({name}, ref) => {
 
     return (
       <Fragment>
-        <Modal isOpen={modalIsOpen} onRequestClose={closeModal} contentLabel="Modal" style={customStyles} portalClassName="modal">
+        <Modal isOpen={modalIsOpen} onRequestClose={closeModal} contentLabel="Video" style={customStyles} portalClassName="modal">
           <button onClick={closeModal} className="close-modal">X</button>
           <h2>{title}</h2>
           <iframe
+          width='100%'
+          height='80%'
           className="embed-responsive-item"
           src={`https://www.youtube.com/embed/${video}`}
           allowFullScreen></iframe>
